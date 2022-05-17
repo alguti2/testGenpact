497b8bb49b374f9837e218a2f607a5ea0bef2755
alguti2 <105685024+alguti2@users.noreply.github.com>
2022-05-16 19:04:51 -0600

    Add files via upload

---
+++ Programming Exercise/programExercise_project/programExercise/programExercise/Main.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace programExercise
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.lcount = new System.Windows.Forms.Label();
+            this.btnExamine = new System.Windows.Forms.Button();
+            this.DGVMonitor = new System.Windows.Forms.DataGridView();
+            this.tarea1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.salida = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Thread1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DGVMonitor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lcount
+            // 
+            this.lcount.AutoSize = true;
+            this.lcount.Location = new System.Drawing.Point(302, 326);
+            this.lcount.Name = "lcount";
+            this.lcount.Size = new System.Drawing.Size(90, 13);
+            this.lcount.TabIndex = 16;
+            this.lcount.Text = "Processed files: 0";
+            // 
+            // btnExamine
+            // 
+            this.btnExamine.Location = new System.Drawing.Point(328, 43);
+            this.btnExamine.Name = "btnExamine";
+            this.btnExamine.Size = new System.Drawing.Size(64, 23);
+            this.btnExamine.TabIndex = 15;
+            this.btnExamine.Text = "Examine";
+            this.btnExamine.UseVisualStyleBackColor = true;
+            this.btnExamine.Click += new System.EventHandler(this.btnExamine_Click);
+            // 
+            // DGVMonitor
+            // 
+            this.DGVMonitor.AllowUserToAddRows = false;
+            this.DGVMonitor.AllowUserToDeleteRows = false;
+            this.DGVMonitor.AllowUserToResizeRows = false;
+            this.DGVMonitor.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.DGVMonitor.BackgroundColor = System.Drawing.Color.White;
+            this.DGVMonitor.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.DGVMonitor.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.DGVMonitor.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGVMonitor.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.tarea1,
+            this.salida});
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(219)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.DGVMonitor.DefaultCellStyle = dataGridViewCellStyle1;
+            this.DGVMonitor.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.DGVMonitor.Location = new System.Drawing.Point(13, 109);
+            this.DGVMonitor.Name = "DGVMonitor";
+            this.DGVMonitor.ReadOnly = true;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.DGVMonitor.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.DGVMonitor.RowHeadersVisible = false;
+            this.DGVMonitor.Size = new System.Drawing.Size(379, 211);
+            this.DGVMonitor.TabIndex = 14;
+            // 
+            // tarea1
+            // 
+            this.tarea1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.tarea1.HeaderText = "File";
+            this.tarea1.Name = "tarea1";
+            this.tarea1.ReadOnly = true;
+            this.tarea1.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.tarea1.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // salida
+            // 
+            this.salida.FillWeight = 350F;
+            this.salida.HeaderText = "Execution date";
+            this.salida.Name = "salida";
+            this.salida.ReadOnly = true;
+            this.salida.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.salida.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.salida.Width = 170;
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(13, 46);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(309, 20);
+            this.txtPath.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(253, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Please put the path of the folder you want to monitor";
+            // 
+            // Thread1
+            // 
+            this.Thread1.Interval = 1000;
+            this.Thread1.Tick += new System.EventHandler(this.Thread1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::programExercise.Properties.Resources.icono_information2;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 10);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(34, 30);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Image = global::programExercise.Properties.Resources.ejecutar_icon1;
+            this.btnStart.Location = new System.Drawing.Point(328, 72);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(64, 31);
+            this.btnStart.TabIndex = 12;
+            this.btnStart.Text = "Start";
+            this.btnStart.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Image = global::programExercise.Properties.Resources.stop_icon1;
+            this.btnStop.Location = new System.Drawing.Point(272, 72);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(50, 31);
+            this.btnStop.TabIndex = 11;
+            this.btnStop.Text = "Stop";
+            this.btnStop.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 349);
+            this.Controls.Add(this.lcount);
+            this.Controls.Add(this.btnExamine);
+            this.Controls.Add(this.DGVMonitor);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Folder Watcher";
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DGVMonitor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lcount;
+        private System.Windows.Forms.Button btnExamine;
+        internal System.Windows.Forms.DataGridView DGVMonitor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tarea1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn salida;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer Thread1;
+    }
+}
+
---
+++ Programming Exercise/programExercise_project/programExercise/programExercise/Main.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.Office.Interop.Excel;
+
+namespace programExercise
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            txtPath.Clear();
+            txtPath.ReadOnly = true;
+            btnStop.Enabled = false;
+            btnStart.Enabled = false;
+            this.Height = 144;
+            btnExamine.Focus();
+            Thread1.Enabled = false;
+            DGVMonitor.Rows.Clear();
+        }
+
+        private void btnExamine_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            folder.Description = "Select the folder";
+            folder.ShowDialog();
+            if (folder.SelectedPath != "")
+            {
+                txtPath.Text = folder.SelectedPath;
+                btnStart.Enabled = true;
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            btnExamine.Enabled = false;
+            Thread1.Enabled = true;
+            btnStop.Enabled = true;
+            btnStart.Enabled = false;
+            DGVMonitor.Rows.Clear();
+
+        }
+
+
+
+
+        string monitor()
+        {
+            try
+            {
+                string pathProcessed = txtPath.Text + "\\Processed";
+                string pathNotApl = txtPath.Text + "\\Not applicable";
+                string masterBook = txtPath.Text + "\\MasterWorkBook.xlsx";
+                string resultcreate = createDirectoryAndMasterBook(pathProcessed, pathNotApl, masterBook);
+
+
+                if (resultcreate == "ok")
+                {
+                    string[] files = Directory.GetFiles(txtPath.Text, "*.xls*");
+                    foreach (var file in files)
+                    {
+                        if (!Path.GetFileName(file).Contains("MasterWorkBook"))
+                        {
+                            string copyExcel = copySheetsNew(file, masterBook);
+                            if (copyExcel == "ok")
+                            {
+                                string fileMoving = fileMove(file, pathProcessed);
+                                if (fileMoving == "ok")
+                                {
+
+                                    DGVMonitor.Rows.Add(Path.GetFileName(file), DateTime.Now.ToString());
+                                    this.Height = 388;
+                                }
+                                else
+                                {
+                                    return "An error occurred while moving the files in Excel format: " + fileMoving;
+
+                                }
+                            }
+                            else
+                            {
+                                return "An error occurred when copying the sheets to the master workbook: " + copyExcel;
+
+                            }
+
+
+                        }
+
+                    }
+                    string[] files2 = Directory.GetFiles(txtPath.Text);
+
+                    foreach (var file2 in files2)
+                    {
+                        if (!Path.GetFileName(file2).Contains("MasterWorkBook"))
+                        {
+                            string fileMoving = fileMove(file2, pathNotApl);
+                            if (fileMoving == "ok")
+                            {
+                                DGVMonitor.Rows.Add(Path.GetFileName(file2), DateTime.Now.ToString());
+                                this.Height = 388;
+                            }
+                            else
+                            {
+                                return "An error occurred while moving the files: " + fileMoving;
+
+                            }
+
+                        }
+                    }
+                }
+                else
+                {
+                    return "An error occurred in the creation of the folders and master book: " + resultcreate;
+                }
+
+                lcount.Text = "Processed files: " + DGVMonitor.Rows.Count;
+                return "ok";
+            }
+            catch (Exception e)
+            {
+                return e.ToString();
+            }
+
+        }
+
+
+        void clear()
+        {
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+            btnExamine.Enabled = true;
+            DGVMonitor.Rows.Clear();
+            lcount.Text = "Processed files: 0";
+            Thread1.Enabled = false;
+            this.Height = 144;
+        }
+
+
+        string createDirectoryAndMasterBook(string fileProcessed, string fileNotApli, string fileMaster)
+        {
+            Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+            try
+            {
+                if (!Directory.Exists(fileProcessed))
+                {
+                    Directory.CreateDirectory(fileProcessed);
+                }
+
+                if (!Directory.Exists(fileNotApli))
+                {
+                    Directory.CreateDirectory(fileNotApli);
+                }
+
+                if (!File.Exists(fileMaster))
+                {
+
+                    excel.Visible = false;
+                    excel.DisplayAlerts = false;
+                    Microsoft.Office.Interop.Excel.Workbook worKbooK = excel.Workbooks.Add(Type.Missing);
+                    worKbooK.SaveAs(fileMaster);
+                    worKbooK.Close();
+                }
+                string[] files = Directory.GetFiles(fileProcessed);
+                string[] files2 = Directory.GetFiles(fileNotApli);
+                if (DGVMonitor.Rows.Count == 0 && (files.Length > 0 || files2.Length > 0))
+                {
+                    foreach (var file in files)
+                    {
+                        DateTime fileCreatedDate = File.GetLastWriteTime(file);
+                        DGVMonitor.Rows.Add(Path.GetFileName(file), fileCreatedDate.ToString());
+                    }
+
+                    foreach (var file2 in files2)
+                    {
+                        DateTime fileCreatedDate = File.GetLastWriteTime(file2);
+                        DGVMonitor.Rows.Add(Path.GetFileName(file2), fileCreatedDate.ToString());
+                    }
+                    this.Height = 388;
+                }
+
+                return "ok";
+            }
+
+            catch (Exception e)
+            {
+                return e.ToString();
+            }
+            finally
+            {
+                excel.DisplayAlerts = false;
+                excel.Quit();
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(excel);
+            }
+
+        }
+
+        string copySheetsNew(String fileFrom, string fileto)
+        {
+            Microsoft.Office.Interop.Excel.Application app = new Microsoft.Office.Interop.Excel.Application();
+            try
+            {
+                Object defaultArg = Type.Missing;
+                Workbook curWorkBook = app.Workbooks.Open(fileFrom, defaultArg, defaultArg, defaultArg, defaultArg, defaultArg, defaultArg, defaultArg, defaultArg, defaultArg, defaultArg, defaultArg, defaultArg, defaultArg, defaultArg);
+                Workbook destWorkbook = app.Workbooks.Open(fileto, defaultArg, false, defaultArg, defaultArg, defaultArg, defaultArg, defaultArg, defaultArg, defaultArg, defaultArg, defaultArg, defaultArg, defaultArg, defaultArg);
+                int count = 1;
+                foreach (Worksheet sheet in curWorkBook.Worksheets)
+                {
+                    curWorkBook.Worksheets[count].Copy(destWorkbook.Worksheets[count]);
+                    count++;
+                }
+
+                destWorkbook.Save();
+                destWorkbook.Close(defaultArg, defaultArg, defaultArg);
+                curWorkBook.Save();
+                curWorkBook.Close(defaultArg, defaultArg, defaultArg);
+                return "ok";
+            }
+            catch (Exception e)
+            {
+                return e.ToString();
+            }
+            finally
+            {
+                app.DisplayAlerts = false;
+                app.Quit();
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(app);
+            }
+
+        }
+
+        string fileMove(string pathStart, string pathEnd)
+        {
+
+            try
+            {
+
+                File.Move(pathStart, pathEnd + "\\" + Path.GetFileName(pathStart));
+
+                return "ok";
+            }
+            catch (System.IO.IOException)
+            {
+                File.Replace(pathStart, pathEnd + "\\" + Path.GetFileName(pathStart), null, false);
+                return "ok";
+            }
+
+            catch (Exception e)
+            {
+
+                return e.ToString();
+            }
+
+
+
+
+        }
+
+        private void Thread1_Tick(object sender, EventArgs e)
+        {
+
+            string monitoring = monitor();
+            if (monitoring != "ok")
+            {
+                clear();
+                MessageBox.Show(monitoring, "File Watcher", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            Thread1.Enabled = false;
+            btnStart.Enabled = true;
+            btnExamine.Enabled = true;
+            btnStop.Enabled = false;
+        }
+
+     
+    }
+
+
+
+}
+
